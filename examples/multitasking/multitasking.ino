42aecf922b03b8343b52ff0afa473672e4ac123f
Lennart Hennigs <mail@lennarthennigs.de>
2018-03-28 13:00:54 +0200

    formatting

--- examples/multitasking/multitasking.ino
+++ examples/multitasking/multitasking.ino
@@ -42,10 +42,10 @@ void setup() {
     pinMode(LED1_PIN, OUTPUT);
     pinMode(LED2_PIN, OUTPUT);
 
-    fsm_led1.add_timed_transition(&state_led1_off, &state_led1_on, 1000);
-    fsm_led1.add_timed_transition(&state_led1_on, &state_led1_off, 3000);
-    fsm_led2.add_timed_transition(&state_led2_off, &state_led2_on, 1000);
-    fsm_led2.add_timed_transition(&state_led2_on, &state_led2_off, 2000);
+    fsm_led1.add_timed_transition(&state_led1_off,  &state_led1_on,     1000);
+    fsm_led1.add_timed_transition(&state_led1_on,   &state_led1_off,    3000);
+    fsm_led2.add_timed_transition(&state_led2_off,  &state_led2_on,     1000);
+    fsm_led2.add_timed_transition(&state_led2_on,   &state_led2_off,    2000);
 }
 
 
