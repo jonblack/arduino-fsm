a0a45ae68d9bfa4b8012a01072d5c4c937d97cfe
Lennart Hennigs <mail@lennarthennigs.de>
2022-04-18 09:17:46 +0200

    updated example to show both types of transitions

--- examples/fsm_graph/fsm_graph.ino
+++ examples/fsm_graph/fsm_graph.ino
@@ -4,18 +4,21 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <Button2.h>
 
 /////////////////////////////////////////////////////////////////
 
-#define WIFI_SSID    "ThisJenIsTheInternet"
+#define WIFI_SSID    "SSID"
 #define WIFI_PASSWD  "*********"
+
+#define BUTTON_WAS_PRESSED  1
 
 /////////////////////////////////////////////////////////////////
 
 void on_red() {
   Serial.println("State: RED");
 }
-
+ 
 void on_yellow() {
   Serial.println("State: YELLOW");
 }
@@ -24,16 +27,24 @@ void on_green() {
   Serial.println("State: GREEN");
 }
 
+void on_button_press() {
+  Serial.println("BUTTON!");
+}
+
 /////////////////////////////////////////////////////////////////
 
-// by giving the two states the same name, the graph will only show three nodes
-State state_yellow_from_red("yellow", on_yellow);
-State state_yellow_from_green("yellow", on_yellow);
-State state_red_light("red", on_red);
-State state_green_light("green", on_green);
-Fsm fsm(&state_red_light);
+State red_light("R", on_red);
+State green_light("G", on_green);
+State yellow_from_red("Y (R)", on_yellow);
+State yellow_from_green("Y(G)", on_yellow);
+
+State button_pressed("BTN", on_button_press);
+
+Fsm fsm(&red_light);
 
 ESP8266WebServer server(80);
+
+Button2 btn;
 
 /////////////////////////////////////////////////////////////////
 
@@ -53,36 +64,54 @@ bool connectToWiFi(char* ssid, char* passwd, int timeout_after =-1) {
 void setup() {
   Serial.begin(9600);
   while (!Serial) {
-    delay(50);
+    delay(100);
   }
   Serial.println();
   Serial.println();
+
   Serial.println("RUNNING: graph visualization\n");
+  // defines a traffic light
+  fsm.add_timed_transition(&red_light, &yellow_from_red, 6000, NULL);
+  fsm.add_timed_transition(&yellow_from_red, &green_light, 1000, NULL);
+  fsm.add_timed_transition(&green_light, &yellow_from_green, 6000, NULL);
+  fsm.add_timed_transition(&yellow_from_green, &red_light, 2000, NULL);
 
-  fsm.add_timed_transition(&state_red_light, &state_yellow_from_red, 6000, NULL, "to yellow");
-  fsm.add_timed_transition(&state_yellow_from_red, &state_green_light, 1000, NULL, "to green");
-  fsm.add_timed_transition(&state_green_light, &state_yellow_from_green, 6000, NULL, "to yellow");
-  fsm.add_timed_transition(&state_yellow_from_green, &state_red_light, 2000, NULL, "to red");
+/*
+  // adds a "press for green" button
+  fsm.add_transition(&yellow_from_red, &button_pressed, BUTTON_WAS_PRESSED, NULL);
+  fsm.add_transition(&yellow_from_green, &button_pressed, BUTTON_WAS_PRESSED, NULL);
+  fsm.add_transition(&red_light, &button_pressed, BUTTON_WAS_PRESSED, NULL);
+  fsm.add_transition(&green_light, &button_pressed, BUTTON_WAS_PRESSED, NULL);
+  fsm.add_timed_transition(&button_pressed, &yellow_from_red, 2000, NULL);
 
-  Serial.println(fsm.get_dot_definition());
+  btn.begin(D4);
+  btn.setTapHandler([](Button2 b) {
+    fsm.trigger(BUTTON_WAS_PRESSED);
+  });
+  
+*/
+  // to print out the .dot definition
+  // Serial.println(fsm.get_dot_definition());
 
-  if (connectToWiFi(WIFI_SSID, WIFI_PASSWD, 10000)) {
-    Serial.print("Open <a href="">http://");
+  if (!connectToWiFi(WIFI_SSID, WIFI_PASSWD, 10000)) {
+    Serial.println(F("Could not connect to WiFi."));
+  } else {
+    Serial.print(F("Open http://"));
     Serial.print(WiFi.localIP());
-    Serial.print("</a> in your browser.\n");
+    Serial.print(F(" in your browser.\n"));
 
     server.on( "/", showGraph);
-    server.onNotFound(handleNotFound);
+    server.onNotFound([](){
+      server.send(404, F("text/plain"), F("File Not Found\n"));
+    });
     server.begin();
-  } else {
-    Serial.println("Could not connect to WiFi.");
   }
-  Serial.println();
 }
 
 /////////////////////////////////////////////////////////////////
 
 void loop() {
+  btn.loop();
   fsm.run_machine();
   server.handleClient();
 }
@@ -90,23 +119,23 @@ void loop() {
 /////////////////////////////////////////////////////////////////
 
 void handleNotFound() {
-  server.send(404, "text/plain", "File Not Found\n");
+  server.send(404, F("text/plain"), F("File Not Found\n"));
 }
 
 /////////////////////////////////////////////////////////////////
 
 void showGraph() {
   String message = "";
-  message += "<html>\n";
-  message += "<head>\n";
-  message += "<meta http-equiv='refresh' content='1'>\n";
-  message += "<title>GraphVizArt</title>\n";
-  message += "</head>\n";
-  message += "<body>\n";
-  message += "<a href='http://gravizo.com/'><img src='https://g.gravizo.com/svg?" + fsm.get_dot_definition() + "'/></a>";
-  message += "</body>\n";
-  message += "</html>\n";
-  server.send ( 200, "text/html", message);
+  message += F("<html>\n");
+  message += F("<head>\n");
+  message += F("<meta http-equiv='refresh' content='1'>\n");
+  message += F("<title>GraphVizArt</title>\n");
+  message += F("</head>\n");
+  message += F("<body>\n");
+  message += F("<a href='http://gravizo.com/'><img src='https://g.gravizo.com/svg?") + fsm.get_dot_definition() + F("'/></a>");
+  message += F("</body>\n");
+  message += F("</html>\n");
+  server.send ( 200, F("text/html"), message);
 }
 
 /////////////////////////////////////////////////////////////////
